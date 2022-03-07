405c33f579e86a1c97b96dfe19295e47dd34324c
sreejesh <sreejesh@kriyatma.com>
2022-03-07 11:59:49 +0530

    added timestamps to roles

--- db/setup.sql
+++ db/setup.sql
@@ -20,7 +20,9 @@ CREATE TABLE user_profiles (
 CREATE TABLE roles (
     id SERIAL PRIMARY KEY,
     machine_name VARCHAR(255) UNIQUE NOT NULL,
-    name VARCHAR(255)  NOT NULL
+    name VARCHAR(255)  NOT NULL,
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE user_roles (
