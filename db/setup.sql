29f940d3b519be29bcf7fcdbec76e56e2923b3a9
sreejesh <sreejesh@kriyatma.com>
2022-04-18 12:04:57 +0530

    changes to schema

--- db/setup.sql
+++ db/setup.sql
@@ -1,32 +1,26 @@
+
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
+    fullname VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    mobile VARCHAR(20) NOT NULL,
-    refresh_token TEXT NULL,
+    mobile VARCHAR(10) NOT NULL,
+    refresh_token TEXT NOT NULL,
     status SMALLINT DEFAULT 0 NOT NULL,
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE TABLE user_profiles (
-    id SERIAL PRIMARY KEY,
-    firstname VARCHAR(255) NOT NULL,
-    lastname VARCHAR(255) NOT NULL,
-    user_id INTEGER REFERENCES users (id) ON DELETE CASCADE
-);
-
-
 
 CREATE TABLE roles (
     id SERIAL PRIMARY KEY,
-    machine_name VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255)  NOT NULL,
+    machine_name VARCHAR(255) UNIQUE NOT NULL,
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE TABLE users_roles_roles (
+CREATE TABLE users_roles (
     "usersId" INTEGER REFERENCES users (id),
     "rolesId" INTEGER REFERENCES roles (id)
 );
@@ -42,9 +36,7 @@ INSERT INTO roles (machine_name, name) VALUES
 CREATE TABLE emails_otp (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
-    otp VARCHAR(6) NOT NULL,
-    otp_token TEXT NOT NULL,
-    expiry BIGINT NOT NULL,
+    token TEXT NOT NULL,
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
@@ -59,30 +51,61 @@ CREATE TABLE colors_pallete (
 
 CREATE TABLE brandings (
     id SERIAL PRIMARY KEY,
-    logo TEXT NOT NULL,
+    logo varchar(255) NOT NULL,
     "colorId" INTEGER REFERENCES colors_pallete (id) DEFAULT NULL,
+    "companiesId" INTEGER REFERENCES colors_pallete (id) DEFAULT NULL,
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE companies (
     id SERIAL PRIMARY KEY,
-    url VARCHAR(255) UNIQUE NOT NULL,
     fullname VARCHAR(255) UNIQUE NOT NULL,
-    pan VARCHAR(255) UNIQUE NOT NULL,
-    "brandingId" INTEGER REFERENCES brandings (id) DEFAULT NULL,
+    url VARCHAR(255) UNIQUE NOT NULL,
+    pan VARCHAR(10) UNIQUE NOT NULL,
+    "brandingsId" INTEGER REFERENCES brandings (id) DEFAULT NULL,
     created_by INTEGER REFERENCES users (id) NOT NULL,
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE TABLE branches (
+CREATE TABLE offices (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    branch_address TEXT NOT NULL,
+    address TEXT NOT NULL,
+    gst_no VARCHAR(255) DEFAULT NULL,
     gst_address TEXT DEFAULT NULL,
-    "companyId" INTEGER REFERENCES companies (id) DEFAULT NULL,
+    "companiesId" INTEGER REFERENCES companies (id) DEFAULT NULL,
     created_by INTEGER REFERENCES users (id) NOT NULL,
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
+
+CREATE TABLE units (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    machine_name VARCHAR(255)
+);
+
+CREATE TABLE unit_users (
+    "unitsId" INTEGER REFERENCES units (id),
+    "usersId" INTEGER REFERENCES users (id)
+);
+
+CREATE TABLE companies_users_roles (
+    id SERIAL PRIMARY KEY,
+    "companiesId" INTEGER REFERENCES companies (id),
+    "userId" INTEGER REFERENCES users (id),
+    "roleId" INTEGER REFERENCES roles (id),
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+
+CREATE TABLE offices_users (
+    "officesId" INTEGER REFERENCES offices (id),
+    "usersId" INTEGER REFERENCES users (id)
+);
+ 
+
+
